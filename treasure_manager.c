94c584a1302a0d29f9ca75e7379f6707c1e9344a
Bianca <bianca.mingiuc@student.upt.ro>
2025-04-10 09:22:34 +0300

    List function

--- treasure_manager.c
+++ treasure_manager.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <errno.h>
 #include <string.h>
 #include <fcntl.h>
@@ -25,22 +24,44 @@ typedef struct treasure{
 }Treasure_t;
 
 
-void add_treasure(char *hunt_id){
-  char director_path[MAX_PATH];
-  snprintf(director_path, sizeof(director_path),"%s",hunt_id);
+int check_directory_exists(char *dir_path) {
+    struct stat statbuf;
+    return (stat(dir_path, &statbuf) == 0 && S_ISDIR(statbuf.st_mode)); 
+}
 
-  if(mkdir(director_path,0755)==-1 && errno!=EEXIST){
-    perror("Failed to create hunt directory");
+void start_function(void){
+  if (!check_directory_exists("HUNTS")) {
+        if (mkdir("HUNTS", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1) {
+            perror("Failed to create HUNTS directory");
+            return;
+        }
+        printf("HUNTS directory created\n");
+    } else {
+        printf("HUNTS directory already exists\n");
+    }
+}
+
+
+void add_treasure(char *hunt_id){
+
+  start_function();
+  
+  char director_path[MAX_PATH];
+  snprintf(director_path, sizeof(director_path),"HUNTS/%s",hunt_id);
+
+  if(mkdir(director_path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)==-1 && errno!=EEXIST){
+    perror("Failed to create directory");
     return;
   }
 
   char file_path[MAX_PATH];
   
-  snprintf(file_path, sizeof(file_path),"%400s/treasures.dat",director_path);
+  snprintf(file_path, sizeof(file_path),"HUNTS/%s/treasures.dat", hunt_id);
+  
 
-  int fd=open(file_path, O_WRONLY | O_CREAT | O_APPEND, 0644);
+  int fd=open(file_path, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
   if(fd<0){
-    perror("Failed to open treasures.dat");
+    perror("Failed to open");
     return;
   }
 
@@ -64,22 +85,64 @@ void add_treasure(char *hunt_id){
 
   printf("Add value: ");
   scanf("%d",&t.value);
+  
 
   if(write(fd, &t, sizeof(Treasure_t)) != sizeof(Treasure_t)){
     perror("Failed to write");
     return;
   }
+  
+
+  lseek(fd, 0, SEEK_SET);
+  
 
   printf("Treasure log added successfully!\n");
   char log_message[256];
   snprintf(log_message,sizeof(log_message), "add: id:%s, username: %s", t.id, t.name);
 
-
   close(fd);
 }
 
 
-int main (int argc, char **argv){
+void list_treasures(char *hunt_id){
+  char file_path[MAX_PATH];
+    snprintf(file_path, sizeof(file_path), "HUNTS/%s/treasures.dat", hunt_id);
+
+    // Open the file for reading
+    int fd = open(file_path, O_RDONLY);
+    if (fd < 0) {
+        perror("Failed to open treasures.dat for reading");
+        return;
+    }
+
+    Treasure_t t;
+
+    printf("Listing treasures in hunt: %s\n", hunt_id);
+    int count = 0;
+    while (read(fd, &t, sizeof(Treasure_t)) == sizeof(Treasure_t)) {
+        printf("Treasure %d: \n", ++count);
+        printf("ID: %s\n", t.id);
+        printf("Name: %s\n", t.name);
+        printf("Longitude: %f\n", t.coord.longitude);
+        printf("Latitude: %f\n", t.coord.latitude);
+        printf("Clue: %s\n", t.clue);
+        printf("Value: %d\n", t.value);
+        printf("----------------------------------------\n");
+    }
+
+  
+    if (count == 0) {
+        printf("No treasures found in this hunt.\n");
+    } else if (errno != 0) {
+        perror("Error reading the file");
+    }
+
+    close(fd);
+}
+
+
+
+int main(int argc, char **argv){
   if(argc<3){
     printf("Not enough arguments\n");
     return 1;
@@ -87,6 +150,10 @@ int main (int argc, char **argv){
 
   if(strcmp(argv[1],"add") ==0 ){
     add_treasure(argv[2]);
+  }
+
+  if(strcmp(argv[1],"list") == 0){
+    list_treasures(argv[2]);
   }
   
   return 0;
